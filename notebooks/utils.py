7d34ed84bab3e363bbc5354fccfd359d7e6d2bc4
Blaž Erzar <blaz.erzar@gmail.com>
2026-03-02 02:17:40 +0100

    Create Radiance Field Network notebook

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -3,6 +3,9 @@ import zipfile
 import numpy as np
 from tqdm import tqdm
 
+# Number of float32 values per pixel (x, y, z, azimuth, elevation, r, g, b)
+PIXEL_VALUES = 8
+
 
 def read_ground_truth_zip(file_path: str, verbose=False) -> list[np.ndarray]:
     arrays = []
@@ -14,6 +17,8 @@ def read_ground_truth_zip(file_path: str, verbose=False) -> list[np.ndarray]:
         for file in files:
             if file.endswith('.bin'):
                 buffer_bytes = archive.read(file)
-                arrays.append(np.frombuffer(buffer_bytes, dtype=np.float32))
+                array = np.frombuffer(buffer_bytes, dtype=np.float32)
+                array = array.reshape(-1, PIXEL_VALUES)
+                arrays.append(array)
 
     return arrays
