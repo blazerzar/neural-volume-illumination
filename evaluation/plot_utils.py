18ec7424cf3e68ad3b80c397dd526cfd4557628a
Blaž Erzar <blaz.erzar@gmail.com>
2026-05-29 23:57:38 +0200

    Evaluate performance

---
+++ evaluation/plot_utils.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+colors = [
+    '#FFF5B1',
+    '#FFE3B0',
+    '#FFD2B1',
+    '#FFBAAF',
+    '#A1E6D9',
+    '#B2ECFF',
+    '#B3CDFF',
+    '#AEAFFF',
+]
+
+
+def set_plot_style():
+    plt.rcParams['lines.linewidth'] = 0.8
+    plt.rcParams['font.family'] = 'Serif'
+    plt.rcParams['legend.fontsize'] = 9
+    plt.rcParams['xtick.direction'] = 'in'
+    plt.rcParams['ytick.direction'] = 'in'
+    plt.rcParams['xtick.major.width'] = 0.5
+    plt.rcParams['ytick.major.width'] = 0.5
+    plt.rcParams['axes.labelsize'] = 8
+
+
+def set_legend_style(legend):
+    frame = legend.get_frame()
+    frame.set_edgecolor('black')
+    frame.set_linewidth(0.8)
+    frame.set_boxstyle('Square', pad=0.05)
